c0eaaad0d48dfef1a8a7311307dea0f26bbd6c93
xiuyazhang <xiuyazhang@wdit.com.cn>
2019-07-04 21:34:53 +0800

    first commit

---
+++ src/main/java/com/xiu/fastsparkdev/FastsparkdevApplication.java
@@ -0,0 +1,13 @@
+package com.xiu.fastsparkdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastsparkdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastsparkdevApplication.class, args);
+	}
+
+}
